13033ae7b604813f2dcca0004ebba937bf7f637e
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2022-09-09 08:50:52 -0300

    security group: add egress rule
    Add egress rule as a configuration parameter instead of a hardcoded one.
    
    Related-to: ISSUE#2
    
    Signed-off-by: Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>

--- main.tf
+++ main.tf
@@ -9,33 +9,22 @@ resource "aws_security_group" "security_groups" {
     description      = each.value.ingress.description
     from_port        = each.value.ingress.from_port
     to_port          = each.value.ingress.to_port
-    protocol         = "tcp"
+    protocol         = each.value.ingress.protocol
     cidr_blocks      = each.value.ingress.cidr_blocks
     ipv6_cidr_blocks = each.value.ingress.ipv6_cidr_blocks
   }
 
+  egress {
+    description      = each.value.egress.description
+    from_port        = each.value.egress.from_port
+    to_port          = each.value.egress.to_port
+    protocol         = each.value.egress.protocol
+    cidr_blocks      = each.value.egress.cidr_blocks
+    ipv6_cidr_blocks = each.value.egress.ipv6_cidr_blocks
+  }
+
   tags = {
     Name        = each.value.tags.name
-    Environment = var.environment
-  }
-}
-
-resource "aws_security_group" "allow_all_outbound_traffic_security_group" {
-  name        = "allow_all_outbound_traffic_security_group"
-  description = "Allow all traffic going to the Internet"
-  vpc_id      = var.vpc_id
-
-  egress {
-    description      = "Allow all outbound traffic"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name        = "allow-all-outbound-traffic"
     Environment = var.environment
   }
 }
